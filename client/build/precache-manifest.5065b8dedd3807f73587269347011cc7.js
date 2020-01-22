275e0870306ac2afd21ebcc5ab7d2771fe28e4ea
Uri121 <robinov121@gmail.com>
2020-01-22 12:09:44 +0200

    upload6

---
+++ client/build/precache-manifest.5065b8dedd3807f73587269347011cc7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2ce75acd805c5a3470805b584ff85cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "94d9d99cd25ae44287c4",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "ef23d77cddcac776e7df",
+    "url": "/static/css/main.780a65cf.chunk.css"
+  },
+  {
+    "revision": "94d9d99cd25ae44287c4",
+    "url": "/static/js/2.00885f4a.chunk.js"
+  },
+  {
+    "revision": "e2fc3609d47d90b8cc004bfe15ddf15e",
+    "url": "/static/js/2.00885f4a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ef23d77cddcac776e7df",
+    "url": "/static/js/main.e568ed31.chunk.js"
+  },
+  {
+    "revision": "0281e3aa3ea9fc02c242",
+    "url": "/static/js/runtime-main.b12ff581.js"
+  },
+  {
+    "revision": "75af51438045174dd74514f0a342072d",
+    "url": "/static/media/vector.75af5143.png"
+  }
+]);
