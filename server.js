d18cf3aff655850833c3d70c4e7c4c671ac72aa3
Uri121 <robinov121@gmail.com>
2020-01-22 11:08:52 +0200

    update

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-const dotenv = require("dotenv");
+const dotenv = require("dotenv").config();
 const cors = require("cors");
 const path = require("path"); 
 
-dotenv.config();
 const user = require("./routes/user");
 const list = require("./routes/list");
 const card = require("./routes/card");
@@ -19,7 +18,7 @@ app.use(bodyParser.urlencoded({ extended: true }), bodyParser.json(), cors());
 
 //connect to Mongo
 mongoose
-  .connect(process.env.DB_CONNECT, {
+  .connect(process.env.DB_CONNECT||process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true
@@ -32,7 +31,7 @@ app.use("/user", user);
 app.use("/list", list);
 app.use("/card", card);
 
-if (process.env.NODE_ENV == `production`) {
+if (process.env.NODE_ENV == "production") {
   app.use(express.static("client/build"));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname,"client","build","index.html"));
