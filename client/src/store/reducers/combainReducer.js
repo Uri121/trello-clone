8fbc79704a6aca9292cefb8f5a3df320963ee8d9
Uri121 <robinov121@gmail.com>
2020-01-23 18:04:54 +0200

    1.0.5

--- client/src/store/reducers/combainReducer.js
+++ client/src/store/reducers/combainReducer.js
@@ -1,21 +1,16 @@
-
 import { createStore, applyMiddleware, combineReducers, compose } from "redux";
 import { reducer as formReducer } from "redux-form";
-import thunk from 'redux-thunk';
-import listReducer from './listReducer';
+import thunk from "redux-thunk";
+import listReducer from "./listReducer";
 import authReducer from "./authReducer";
 import errorReducer from "./errorReducer";
 
-
 export const store = createStore(
-    combineReducers({
-      form:formReducer,
-      lists: listReducer,
-      auth: authReducer,
-      error: errorReducer
-    }),
-    compose(
-      applyMiddleware(thunk),
-      window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
-  );
+  combineReducers({
+    form: formReducer,
+    lists: listReducer,
+    auth: authReducer,
+    error: errorReducer
+  }),
+  compose(applyMiddleware(thunk))
+);
